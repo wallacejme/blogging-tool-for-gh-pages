920c8ba1b4aa4e3688a859edfb06ecdbd1b82f7e
Wallace Medeiros <wallacejme@gmail.com>
2018-02-08 14:34:49 -0300

    change third-party libs directory (previously on node_modules/)

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,36 @@ const runSequence = require('run-sequence');
 const es = require('event-stream');
 
 gulp.task('clean', () => {
-  return gulp.src(['docs/'])
+  return gulp.src(['libs/', 'docs/'], { read: false })
     .pipe(clean());
 });
 
+gulp.task('organizeCSSLibs', () => {
+  return gulp.src('node_modules/normalize-css/normalize.css')
+    .pipe(gulp.dest('libs/css'));
+});
+
+gulp.task('organizeJSLibs', () => {
+  return gulp.src('node_modules/jquery/dist/jquery.js')
+    .pipe(gulp.dest('libs/js'));
+})
+
+gulp.task('organizeLibs', ['organizeCSSLibs', 'organizeJSLibs']);
+
 gulp.task('publish', () => {
   return es.merge([
-      gulp.src('index.html'),
-      gulp.src('img/**/*', { base: './' }),
-      gulp.src('css/**/*.css', { base: './' }),
-      gulp.src('js/**/*.js', { base: './' }),
-      gulp.src('libs/**/*.css', { base: './' }),
-      gulp.src('libs/**/*.js', { base: './' }),
-      gulp.src(['node_modules/normalize-css/normalize.css',
-                'node_modules/jquery/dist/jquery.min.js'], { base: './' })
+      gulp.src([
+        'index.html',
+        'img/**/*',
+        'css/**/*.css',
+        'js/**/*.js',
+        'libs/**/*',
+      ], { base: './' } )
     ])
     .pipe(gulp.dest('docs/'));
 });
 
-gulp.task('default', function () {
-	return runSequence('clean', 'publish');
+
+gulp.task('default', () => {
+	return runSequence('clean', 'organizeLibs', 'publish');
 });
