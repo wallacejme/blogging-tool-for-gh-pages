e8cc3221ce9e6e2c6d23d793dd4464e31823ec99
Wallace Medeiros <wallacejme@gmail.com>
2018-02-16 14:00:21 -0300

    gh-page: apply latest changes
    Summary:
    - navbar improvements

--- docs/build/js/script.js
+++ docs/build/js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   
   'use strict';
 
+  /*
+   * Detects page scrolling & show/hide navbar accordingly.
+   */
   var c, 
       currentScrollTop = 0,
       navbar = $('nav');
@@ -21,7 +24,10 @@ $(document).ready(function () {
     c = currentScrollTop;
   });
 
-  $('button#navbarMenuIcon').click(function(element, a1, a2, a3) {    
+  /*
+   * Expand/collapse navbar through interation with menu icon.
+   */
+  $('button#navbarMenuIcon').click(function() {    
     $('nav').toggleClass('expanded');
 
     const menuIcon = $(this).children('i').first();
@@ -35,8 +41,22 @@ $(document).ready(function () {
     }
   });
 
+  /*
+   * Collapse navbar if other page elements are clicked.
+   */
+  $(':not(html, body, #pageHeader, #pageHeader *)').click(function() {
+    console.log($(this));
+    $('nav').removeClass('expanded');
+    const menuIcon = $('button#navbarMenuIcon').children('i').first();
+    menuIcon.removeClass('rotate--clockwise--90deg');
+    menuIcon.text('menu');
+  });
+
+  /*
+   * Automatically remove focus after clicking on buttons & links.
+   */
   $( 'a, button' ).click(function () {
     $(this).blur();
-  })
-  
+  });
+
 });
